f901bbd9e830a18b9a49ae9458e416f2d34723db
Sara Chiriboga <sara.chiriboga.valle@udla.edu.ec>
2026-01-22 03:50:14 -0500

    logout

---
+++ SGRC-Integrador/Controllers/UsuariosController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SGRC_Integrador.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Logout()
+        {
+            // 1. Limpiar la sesión de ASP.NET
+            Session.Clear();
+            Session.Abandon();
+
+            // 2. Limpiar la cookie de autenticación (si usas FormsAuthentication)
+            System.Web.Security.FormsAuthentication.SignOut();
+
+            // 3. Redirigir al Login
+            return RedirectToAction("Login", "Auth");
+        }
+    }
+}
